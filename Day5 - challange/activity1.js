577abb80fdec026a4150aac106eb7d7f9f778f7c
Korubilli venkata sai <korubillivenkatasai@gmail.com>
2025-07-09 18:35:32 +0530

    Update activity1.js

--- Day5 - challange/activity1.js
+++ Day5 - challange/activity1.js
@@ -1,1 +1,20 @@
+const a=7
+const b=2
+const c=1
 
+if(a>b){
+    if(a>c)
+        console.log("A is the Largest");
+    else 
+        console.log("C is Largest");
+
+}
+else if(b>a){
+    if(b>c)
+        console.log("B is the Largest");
+    else 
+        console.log("C is the Largest");
+
+}
+else 
+    console.log("There is no larger number");

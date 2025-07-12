9365e0ea66eb34af5bb7fd7352bb9fee6fc8a8ab
Korubilli venkata sai <korubillivenkatasai@gmail.com>
2025-07-12 21:35:04 +0530

    Update Activity2.js

--- Day7 - challange/Activity2.js
+++ Day7 - challange/Activity2.js
@@ -1,1 +1,13 @@
+const person = {
+  name: "venkat",
+  age: 22,
+  profession: "Student",
+  greet: function() {
+    console.log("Hello, my name is " + this.name);
+  }
+};
+console.log(person.name);       
+console.log(person.age);
+console.log(person.profession); 
+person.greet();  
 
